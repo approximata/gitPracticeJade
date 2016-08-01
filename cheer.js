1372539ecbaecd1135dba1bbd55308c322ec6536
fabicsp <fapqabt@gmail.com>
2016-08-01 16:46:21 +0200

    c4

--- cheer.js
+++ cheer.js
@@ -1,10 +1,6 @@
 'use strict';
 
 function cheers() {
-<<<<<<< HEAD
-  console.log('Greetings');
-=======
   console.log('javascript');
->>>>>>> b5fcdfe62ee7d56e016e89b3c2f0578a9de95938
 }
 cheers()
