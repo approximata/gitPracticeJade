43655bc54a539b597f8b4548cf9fa9951188daa6
bodor mate <bodormate@gmail.com>
2016-08-01 16:59:27 +0200

    a5

--- greet.js
+++ greet.js
@@ -1,4 +1,5 @@
 'use strict';
-function(name){
-  console.log(name);
-}
+function greetings(lastName, firstName){
+  console.log("Hello my name is " + lastName + firstName);
+};
+greetings('mate', 'kovacs');
